5e863a1459b52c513b14008e3f7c83e8c85845ed
Ah-ae <kimaae@gmail.com>
2024-05-19 18:21:03 +0900

    init css base layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "요즘뭐보니",
-  description: "",
+  title: '요즘뭐보니',
+  description: '',
 };
 
 export default function RootLayout({
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`mx-auto max-w-lg shadow-xl bg-gray-100 text-neutral-900 ${inter.className}`}>{children}</body>
     </html>
   );
 }
