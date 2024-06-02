8d021dc944b96d612d9f4f1e6a4e5b0640d1168d
Ah-ae <kimaae@gmail.com>
2024-06-02 18:20:29 +0900

    update: hero Image에 priority 속성 추가 (LCP)

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="min-h-dvh p-6 flex flex-col justify-between items-center">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-        <Image src="/hero_256x256.png" alt="요즘뭐보니 대표 이미지" width={256} height={256} />
+        <Image src="/hero_256x256.png" alt="요즘뭐보니 대표 이미지" width={256} height={256} priority />
         <h1 className="my-2 text-4xl">요즘 뭐 보니</h1>
         <p className="text-2xl">친구들의 책장을 구경해 볼까요?</p>
       </div>
