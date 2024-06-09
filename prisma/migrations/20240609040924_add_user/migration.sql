448f7c4c6380931a525b968973f66ceb3cda1452
Ah-ae <kimaae@gmail.com>
2024-06-09 13:12:01 +0900

    feat: User 스키마 정의

---
+++ prisma/migrations/20240609040924_add_user/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT,
+    "kakao_id" TEXT,
+    "avatar" TEXT,
+    "description" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_kakao_id_key" ON "User"("kakao_id");
