e60d0e9b1f35c9251727431a9ee7d5fe764fd700
Ah-ae <kimaae@gmail.com>
2024-06-09 16:25:55 +0900

    feat: 설정(/settings) 페이지에 로그아웃 로직 구현 (임시)

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,37 @@
-export default function Settings() {
-  return <div>설정</div>;
+import { notFound, redirect } from 'next/navigation';
+import db from '@/lib/db';
+import getSession from '@/lib/session';
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) return user;
+  }
+
+  notFound();
+}
+
+export default async function Settings() {
+  const user = await getUser();
+
+  const logout = async () => {
+    'use server';
+    const session = await getSession();
+    session.destroy();
+    redirect('/');
+  };
+
+  return (
+    <>
+      <h1>반갑습니다, {user?.name} 님!</h1>
+      <form action={logout}>
+        <button>로그아웃</button>
+      </form>
+    </>
+  );
 }
