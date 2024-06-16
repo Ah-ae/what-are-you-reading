c0c4adecd65df5d6594cc33f1a6921b56eeef2ee
Ah-ae <kimaae@gmail.com>
2024-06-16 14:20:24 +0900

    chore: next config에 외부 이미지 사용을 위한 images 설정 추가
    - k.kakaocdn.net : 카카오 소셜 로그인으로 가져온 프로필 이미지
    - search1.kakaocdn.net : 카카오(Daum) 책 검색 API로 가져온 책 표지 섬네일 이미지

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'k.kakaocdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'search1.kakaocdn.net',
+        pathname: '/thumb/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
