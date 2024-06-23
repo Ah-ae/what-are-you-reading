3314b11d685a5798a13db87edeca84cee639cdb0
Ah-ae <kimaae@gmail.com>
2024-06-23 13:34:06 +0900

    refactor: SharedLayout 컴포넌트를 컴포넌트 하위 파일에서 layout.tsx로 이동

--- app/(bookshelf)/yours/layout.tsx
+++
@@ -1,5 +0,0 @@
-import SharedLayout from '@/components/shared-layout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <SharedLayout>{children}</SharedLayout>;
-}
