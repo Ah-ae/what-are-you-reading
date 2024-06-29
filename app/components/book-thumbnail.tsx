f6d20850a5c573c8ad55c6027a1dadd3f928d8d3
Ah-ae <kimaae@gmail.com>
2024-06-29 20:11:46 +0900

    feat: 섬네일 아이템 클릭 토글에 따른 UI 조건부 렌더링 및 selectedItems 업데이트

--- app/components/book-thumbnail.tsx
+++ app/components/book-thumbnail.tsx
@@ -1,4 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
+import { useSearchParams } from 'next/navigation';
+import { CheckCircleIcon } from '@heroicons/react/24/outline';
 
 // thumbnail url 예시: https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038
 // 위의 예시에서 /thumb/R120x174 처럼 url path에 이미지의 width, height 정보가 들어있음.
@@ -17,14 +23,57 @@ function getImageSize(url: string) {
   return { width: null, height: null };
 }
 
-type Props = { thumbnail: string; title: string };
+const SELECTED_ITEMS_KEY = 'selectedItems';
 
-export default function BookThumbnail({ thumbnail, title }: Props) {
+const updateSelectedItemsInSessionStorage = (id: number, isSelected: boolean) => {
+  const items = sessionStorage.getItem(SELECTED_ITEMS_KEY);
+  let updatedItems = items ? JSON.parse(items) : [];
+
+  if (isSelected) {
+    // 세션 스토리지에서 id 삭제
+    updatedItems = updatedItems.filter((itemId: number) => itemId !== id);
+  } else {
+    // 세션 스토리지에 id 추가
+    updatedItems.push(id);
+  }
+
+  sessionStorage.setItem(SELECTED_ITEMS_KEY, JSON.stringify(updatedItems));
+};
+
+type Props = { id: number; thumbnail: string; title: string };
+
+export default function BookThumbnail({ id, thumbnail, title }: Props) {
   const { width, height } = getImageSize(thumbnail);
+  const searchParams = useSearchParams();
+  const currentMode = searchParams.get('mode') || 'view';
+  const [isSelected, setIsSelected] = useState(false);
 
   if (!width || !height) {
     return <span>Invalid thumbnail URL</span>;
   }
 
-  return <Image src={thumbnail} alt={`${title} thumbnail`} className="shadow-lg" width={width} height={height} />;
+  const handleClick = () => {
+    setIsSelected((prev) => !prev);
+    updateSelectedItemsInSessionStorage(id, isSelected);
+  };
+
+  if (currentMode === 'view') {
+    sessionStorage.removeItem(SELECTED_ITEMS_KEY);
+  }
+
+  return currentMode === 'view' ? (
+    <Link href={`/mine/${id}`} className="cursor-pointer">
+      <Image src={thumbnail} alt={title} className="shadow-lg" width={width} height={height} />
+    </Link>
+  ) : isSelected ? (
+    <div className="relative cursor-pointer" onClick={handleClick}>
+      <div className="absolute inset-0 bg-gray-300 opacity-50" /> {/* dimmed overlay */}
+      <CheckCircleIcon className="absolute bottom-2 left-1/2 transform -translate-x-1/2 size-5 bg-red-600 text-white rounded-full" />
+      <Image src={thumbnail} alt={title} className="shadow-lg" width={width} height={height} />
+    </div>
+  ) : (
+    <div className="cursor-pointer" onClick={handleClick}>
+      <Image src={thumbnail} alt={title} className="shadow-lg" width={width} height={height} />
+    </div>
+  );
 }
