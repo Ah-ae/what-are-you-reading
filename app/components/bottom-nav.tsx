fcd709c264a369b6ea2fc5842069c462aef70d46
Ah-ae <kimaae@gmail.com>
2024-06-29 12:05:54 +0900

    update: 하단 네비게이션 시맨틱 마크업으로 변경 (div → nav)

--- app/components/bottom-nav.tsx
+++ app/components/bottom-nav.tsx
@@ -1,12 +1,9 @@
-import { MAX_WIDTH } from '@/constants/style';
 import NavLinks from './nav-links';
 
 export default function BottomNav() {
   return (
-    <div
-      className={`w-full fixed bottom-0 left-1/2 transform -translate-x-1/2 ${MAX_WIDTH} px-3 md:px-8 py-2 flex justify-between bg-gray-50 dark:bg-zinc-700 border-t border-gray-200 dark:border-zinc-600`}
-    >
+    <nav>
       <NavLinks />
-    </div>
+    </nav>
   );
 }
