b18018573dfd8f8195ec63aaa0cca17ec3c5a32f
Ah-ae <kimaae@gmail.com>
2024-06-29 12:05:54 +0900

    update: 하단 네비게이션 시맨틱 마크업으로 변경 (div → nav)

--- app/components/nav-links.tsx
+++ app/components/nav-links.tsx
@@ -8,6 +8,7 @@ import {
   UsersIcon as SolidUsersIcon,
   Cog6ToothIcon as SolidCog6ToothIcon,
 } from '@heroicons/react/24/solid';
+import { MAX_WIDTH } from '@/constants/style';
 
 const links = [
   { name: '내 책장', href: '/mine', defaultIcon: BookOpenIcon, selectedIcon: SolidBookOpenIcon },
@@ -24,23 +25,23 @@ export default function NavLinks() {
   const pathname = usePathname();
 
   return (
-    <>
+    <ul
+      className={`w-full fixed bottom-0 left-1/2 transform -translate-x-1/2 ${MAX_WIDTH} px-3 md:px-8 py-2 flex justify-between bg-gray-50 dark:bg-zinc-700 border-t border-gray-200 dark:border-zinc-600`}
+    >
       {links.map((link, index) => {
         const DefaultLinkIcon = link.defaultIcon;
         const SelectedLinkIcon = link.selectedIcon;
         return (
-          <Link
-            key={`link-${index}`}
-            href={link.href}
-            className="h-10 md:h-12 px-3 flex-center grow gap-2 rounded-md text-sm font-medium"
-          >
-            <div className="flex flex-col items-center">
-              {pathname === link.href ? <SelectedLinkIcon className="w-6" /> : <DefaultLinkIcon className="w-6" />}
-              <p>{link.name}</p>
-            </div>
-          </Link>
+          <li key={`link-${index}`} className="h-10 md:h-12 px-3 flex-center grow gap-2 rounded-md text-sm font-medium">
+            <Link href={link.href}>
+              <div className="flex flex-col items-center">
+                {pathname === link.href ? <SelectedLinkIcon className="w-6" /> : <DefaultLinkIcon className="w-6" />}
+                <span>{link.name}</span>
+              </div>
+            </Link>
+          </li>
         );
       })}
-    </>
+    </ul>
   );
 }
