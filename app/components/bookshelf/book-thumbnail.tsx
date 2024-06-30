94a5e5d055f28d9cb627251445756336e81b7440
Ah-ae <kimaae@gmail.com>
2024-06-30 10:42:16 +0900

    update: session storage 대신 url params에 selectedItems 값 저장
    - session storage에 저장시 Header 컴포넌트에서 실시간으로 값에 접근할 수 없었음
    - onClick 이벤트 핸들러에서서만 실시간 접근 가능했음

--- app/components/bookshelf/book-thumbnail.tsx
+++ app/components/bookshelf/book-thumbnail.tsx
@@ -3,8 +3,9 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { CheckCircleIcon } from '@heroicons/react/24/outline';
+import { SELECTED_ITEMS_KEY } from '@/constants';
 
 // thumbnail url 예시: https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038
 // 위의 예시에서 /thumb/R120x174 처럼 url path에 이미지의 width, height 정보가 들어있음.
@@ -23,28 +24,13 @@ function getImageSize(url: string) {
   return { width: null, height: null };
 }
 
-const SELECTED_ITEMS_KEY = 'selectedItems';
-
-const updateSelectedItemsInSessionStorage = (id: number, isSelected: boolean) => {
-  const items = sessionStorage.getItem(SELECTED_ITEMS_KEY);
-  let updatedItems = items ? JSON.parse(items) : [];
-
-  if (isSelected) {
-    // 세션 스토리지에서 id 삭제
-    updatedItems = updatedItems.filter((itemId: number) => itemId !== id);
-  } else {
-    // 세션 스토리지에 id 추가
-    updatedItems.push(id);
-  }
-
-  sessionStorage.setItem(SELECTED_ITEMS_KEY, JSON.stringify(updatedItems));
-};
-
 type Props = { id: number; thumbnail: string; title: string };
 
 export default function BookThumbnail({ id, thumbnail, title }: Props) {
   const { width, height } = getImageSize(thumbnail);
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
   const currentMode = searchParams.get('mode') || 'view';
   const [isSelected, setIsSelected] = useState(false);
 
@@ -57,22 +43,43 @@ export default function BookThumbnail({ id, thumbnail, title }: Props) {
   }
 
   const handleClick = () => {
+    // local state isSelected 값 업데이트
     setIsSelected((prev) => !prev);
-    updateSelectedItemsInSessionStorage(id, isSelected);
+
+    // url params에 selectedItems 값 업데이트
+    const params = new URLSearchParams(searchParams);
+    const items = params.get(SELECTED_ITEMS_KEY);
+    let updatedItems = items ? items.split(',').map(Number) : [];
+
+    if (isSelected) {
+      // id 삭제
+      updatedItems = updatedItems.filter((itemId: number) => itemId !== id);
+    } else {
+      // id 추가
+      updatedItems.push(id);
+    }
+
+    if (updatedItems.length > 0) {
+      params.set(SELECTED_ITEMS_KEY, updatedItems.join(','));
+    } else {
+      params.delete(SELECTED_ITEMS_KEY);
+    }
+
+    replace(`${pathname}?${params.toString()}`);
   };
 
   return currentMode === 'view' ? (
     <Link href={`/mine/${id}`} className="cursor-pointer">
       <Image src={thumbnail} alt={title} className="shadow-lg" width={width} height={height} />
     </Link>
-  ) : isSelected ? (
-    <div className="relative cursor-pointer" onClick={handleClick}>
-      <div className="absolute inset-0 bg-gray-300 opacity-50" /> {/* dimmed overlay */}
-      <CheckCircleIcon className="absolute bottom-2 left-1/2 transform -translate-x-1/2 size-5 bg-red-600 text-white rounded-full" />
-      <Image src={thumbnail} alt={title} className="shadow-lg" width={width} height={height} />
-    </div>
   ) : (
-    <div className="cursor-pointer" onClick={handleClick}>
+    <div className="relative cursor-pointer" onClick={handleClick}>
+      {isSelected && (
+        <>
+          <div className="absolute inset-0 bg-gray-300 opacity-50" /> {/* dimmed overlay */}
+          <CheckCircleIcon className="absolute bottom-2 left-1/2 transform -translate-x-1/2 size-5 bg-red-600 text-white rounded-full" />
+        </>
+      )}
       <Image src={thumbnail} alt={title} className="shadow-lg" width={width} height={height} />
     </div>
   );
