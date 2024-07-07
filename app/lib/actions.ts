9d84bd36b5184e6edd79635225dad8611fd92acc
Ah-ae <kimaae@gmail.com>
2024-07-07 20:27:25 +0900

    feat: deleteBooks 서버 액션 함수 작성

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,22 +4,19 @@ import db from '@/lib/db';
 import { revalidatePath } from 'next/cache';
 
 export async function deleteBooks(ids: number[]) {
-  console.log('delete books: ', ids);
-  // TODO: 복수의 데이터 일괄 삭제 서버 액션
-  //! NOT WORKING
-  // 현재는 DELETE가 아닌 POST 메소드가 호출되고 있음 : POST /mine?mode=edit&items=2 200
-  //   try {
-  //     const deleteResult = await db.book.deleteMany({
-  //       where: {
-  //         id: {
-  //           in: ids,
-  //         },
-  //       },
-  //     });
-  //     console.log(`Deletion result:`, deleteResult);
-  //     revalidatePath('/mine');
-  //     return { message: 'Deleted Books.' };
-  //   } catch (error) {
-  //     return { message: 'Database Error: Failed to Delete Books.' };
-  //   }
+  try {
+    await db.book.deleteMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+    console.log('Books deleted successfully');
+    revalidatePath('/mine');
+    return { message: 'Deleted Books.' };
+  } catch (error) {
+    console.error('Error deleting books:', error);
+    return { message: 'Database Error: Failed to Delete Books.' };
+  }
 }
