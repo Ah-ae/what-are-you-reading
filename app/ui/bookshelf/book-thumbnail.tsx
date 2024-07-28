afcf00e1e093d65e5cebb92a8bd2f9f7ac8fbafb
Ah-ae <kimaae@gmail.com>
2024-07-28 16:05:14 +0900

    delete: BookThumbnail에서 getImageSize 함수 삭제 (utils에서 import)

--- app/ui/bookshelf/book-thumbnail.tsx
+++ app/ui/bookshelf/book-thumbnail.tsx
@@ -3,25 +3,9 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { CheckCircleIcon } from '@heroicons/react/24/outline';
-import { useBookThumbnail } from '@/utils/hooks/useBookThumbnail';
 import InvalidThumbnail from '@/ui//invalid-thumbnail';
-
-// thumbnail url 예시: https://search1.kakaocdn.net/thumb/R120x174.q85/?fname=http%3A%2F%2Ft1.daumcdn.net%2Flbook%2Fimage%2F1467038
-// 위의 예시에서 /thumb/R120x174 처럼 url path에 이미지의 width, height 정보가 들어있음.
-// 이를 추출하기 위한 헬퍼 함수
-function getImageSize(url: string) {
-  const regex = /\/thumb\/R(\d+)x(\d+)/;
-  const match = url.match(regex);
-
-  if (match) {
-    return {
-      width: Number(match[1]),
-      height: Number(match[2]),
-    };
-  }
-
-  return { width: null, height: null };
-}
+import { useBookThumbnail } from '@/utils/hooks/useBookThumbnail';
+import { getImageSize } from '@/utils/image';
 
 type Props = { id: number; thumbnail: string; title: string };
 
