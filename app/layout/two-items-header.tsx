156e4264992a83d081b08fe042221a8cc9bf206b
Ah-ae <kimaae@gmail.com>
2024-08-25 17:25:54 +0900

    style: header layout title의 dark 모드 텍스트 색상 변경

--- app/layout/two-items-header.tsx
+++ app/layout/two-items-header.tsx
@@ -9,9 +9,9 @@ type Props = {
 export default function HeaderLayout({ backButtonText = '뒤로', title }: Props) {
   return (
     <header
-      className={`w-full fixed top-0 left-1/2 transform -translate-x-1/2 ${MAX_WIDTH} p-4 flex justify-center items-center bg-gray-50 dark:bg-zinc-700 border-b border-gray-200 dark:border-zinc-600 *:dark:text-blue-500`}
+      className={`w-full fixed top-0 left-1/2 transform -translate-x-1/2 ${MAX_WIDTH} p-4 flex justify-center items-center bg-gray-50 dark:bg-zinc-700 border-b border-gray-200 dark:border-zinc-600`}
     >
-      <BackButton className="absolute left-4">{backButtonText}</BackButton>
+      <BackButton className="absolute left-4 dark:text-blue-500">{backButtonText}</BackButton>
       <h2 className="text-xl font-medium dark:text-neutral-200">{title}</h2>
     </header>
   );
