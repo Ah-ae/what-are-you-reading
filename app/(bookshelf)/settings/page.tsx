9e5d4239d3b168267242b4da4f9a76533a01e283
Ah-ae <kimaae@gmail.com>
2024-08-25 15:23:23 +0900

    feat: 설정 페이지 마크업

--- app/(bookshelf)/settings/page.tsx
+++ app/(bookshelf)/settings/page.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
 import getSession from '@/lib/session';
-import { getUser } from '@/lib/data';
+import HeaderLayout from '@/layout/header';
+import BackButton from '@/ui/back-button';
+
+const containerStyles = 'px-4 py-3 flex justify-between border-y-[1px] dark:border-neutral-700';
+const beforePseudoElementStyles =
+  'before:content-[attr(data-before)] before:absolute before:-translate-y-10 before:text-neutral-500';
 
 export default async function Settings() {
-  const user = await getUser();
-
   const logout = async () => {
     'use server';
     const session = await getSession();
@@ -15,11 +18,36 @@ export default async function Settings() {
 
   return (
     <>
-      <h1>반갑습니다, {user?.name} 님!</h1>
-      {user?.avatar && <Image src={user?.avatar ?? ''} alt="사용자 프로필 이미지" width={48} height={48} />}
-      <form action={logout}>
-        <button>로그아웃</button>
-      </form>
+      <HeaderLayout>
+        <BackButton />
+        <div>
+          <h2 className="text-xl font-medium dark:text-neutral-200">설정</h2>
+        </div>
+        <div />
+        <div />
+      </HeaderLayout>
+
+      <div className="flex flex-col gap-12 bg-zinc-100 dark:bg-zinc-900  *:bg-white *:dark:bg-zinc-800">
+        <div>
+          <div className={containerStyles}>
+            <span>계정</span>
+            <ChevronRightIcon className="size-5 text-zinc-400" />
+          </div>
+          <div className={containerStyles}>
+            <span>친구</span>
+            <ChevronRightIcon className="size-5 text-zinc-400" />
+          </div>
+        </div>
+
+        <div data-before="앱 관리" className={`${beforePseudoElementStyles} ${containerStyles}`}>
+          <span>버전</span>
+          <span className="text-main-theme-color dark:text-blue-500">0.0.0</span>
+        </div>
+
+        <form action={logout} className={containerStyles}>
+          <button className="text-zinc-400">로그아웃</button>
+        </form>
+      </div>
     </>
   );
 }
