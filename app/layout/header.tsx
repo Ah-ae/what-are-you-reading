fbbf71b1a6c5a62b9fbb81483c2e3a78a55ab980
Ah-ae <kimaae@gmail.com>
2024-08-25 11:39:21 +0900

    style: headerLayout에 sticky position 추가

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function HeaderLayout({ children }: Props) {
   return (
-    <header className="p-4 flex justify-between items-center bg-gray-50 dark:bg-zinc-700 border-b border-gray-200 dark:border-zinc-600 *:dark:text-blue-500">
+    <header className="sticky top-0 z-10 p-4 flex justify-between items-center bg-gray-50 dark:bg-zinc-700 border-b border-gray-200 dark:border-zinc-600 *:dark:text-blue-500">
       {children}
     </header>
   );
