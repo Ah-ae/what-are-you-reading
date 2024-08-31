709858c1fe529a23b92dd86dfaafcd440aa4248a
Ah-ae <kimaae@gmail.com>
2024-08-31 18:16:49 +0900

    update: public 폴더에 있는 이미지 src 절대경로로 변경

--- app/ui/kakao-login.tsx
+++ app/ui/kakao-login.tsx
@@ -7,7 +7,12 @@ export default function KakaoLogin() {
       href="/kakao/start"
       className="w-full h-10 flex-center gap-3 bg-[#FEE500] hover:bg-[#FADA0A] text-[#3C1E1E] font-medium rounded-md text-center transition-colors"
     >
-      <Image src="/kakao-icon.png" alt="KakaoTalk Icon" width={24} height={24} />
+      <Image
+        src={process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL + '/kakao-icon.png' : '/kakao-icon.png'}
+        alt="KakaoTalk Icon"
+        width={24}
+        height={24}
+      />
       <span>카카오 로그인</span>
     </Link>
   );
