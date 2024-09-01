6ee3cbc2eac090fe03dfbfeccec38a238b6620e0
Ah-ae <kimaae@gmail.com>
2024-09-01 17:19:52 +0900

    update: APP_BASE_URL에 env 파일에 있는 PUBLIC_URL 사용

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,2 +1,2 @@
 const IS_DEV_MODE = process.env.NODE_ENV === 'development';
-export const APP_BASE_URL = IS_DEV_MODE ? 'http://localhost:3000' : 'https://what-are-you-reading.vercel.app';
+export const APP_BASE_URL = IS_DEV_MODE ? 'http://localhost:3000' : process.env.PUBLIC_URL;
