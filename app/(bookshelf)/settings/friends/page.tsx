16d5ad6b8ec145f7b668fa4808a0a7e0ba41480c
Ah-ae <kimaae@gmail.com>
2024-09-01 17:31:43 +0900

    style: 설정 > 친구 관리 페이지 다크모드 배경색 지정

--- app/(bookshelf)/settings/friends/page.tsx
+++ app/(bookshelf)/settings/friends/page.tsx
@@ -5,7 +5,7 @@ export default function FriendsSettings() {
     <>
       <HeaderLayout backButtonText="설정" title="친구 관리" />
 
-      <div className="h-full py-4 text-center bg-white">
+      <div className="h-full py-4 text-center bg-white dark:bg-zinc-950 dark:text-gray-100">
         <p>준비 중인 기능입니다.</p>
         <p>조금만 기다려 주세요 👀</p>
       </div>
