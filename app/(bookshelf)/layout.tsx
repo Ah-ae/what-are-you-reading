01116e0315875980096a92b191acf06aaa38145a
Ahae KIM <115610668+Ah-ae@users.noreply.github.com>
2024-09-01 15:10:52 +0900

    fix: h-dvh 제거 (#54)

--- app/(bookshelf)/layout.tsx
+++ app/(bookshelf)/layout.tsx
@@ -3,7 +3,7 @@ import BottomNav from '@/ui/nav/bottom-nav';
 export default function SharedLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <section className="h-dvh pt-12 pb-12">{children}</section>
+      <section className="pt-12 pb-12">{children}</section>
       <BottomNav />
     </>
   );
