8ea09af9957f63aa61be41bb665971c1a52f65eb
Ah-ae <kimaae@gmail.com>
2024-09-01 14:29:58 +0900

    style: `/settings` 및 하위페이지 배경색 layout에서 컨트롤

--- app/(bookshelf)/settings/friends/page.tsx
+++ app/(bookshelf)/settings/friends/page.tsx
@@ -5,7 +5,7 @@ export default function FriendsSettings() {
     <>
       <HeaderLayout backButtonText="설정" title="친구 관리" />
 
-      <div className="py-4 text-center">
+      <div className="h-dvh py-4 text-center bg-white">
         <p>준비 중인 기능입니다.</p>
         <p>조금만 기다려 주세요 👀</p>
       </div>
