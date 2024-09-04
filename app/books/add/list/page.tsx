9d883b14bcc6bfebfaa89ff6e384c5bdf4c1d740
Ah-ae <kimaae@gmail.com>
2024-09-04 20:48:01 +0900

    update: 책 검색 목록 페이지 헤더에 '완료' 버튼 추가

--- app/books/add/list/page.tsx
+++ app/books/add/list/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import HeaderLayout from '@/layout/header';
 import BookList from '@/ui/books/cards';
 import { searchBooks, checkOwnedBooks } from '@/books/add/list/actions';
@@ -25,7 +26,7 @@ export default async function List({
 
   return (
     <>
-      <HeaderLayout backButtonText="검색 목록" />
+      <HeaderLayout backButtonText="검색 목록" rightItem={<Link href="/mine">완료</Link>} />
 
       {/* Note: `pt-12` - header height만큼 공간 확보 + page section의 자체 패딩 */}
       <section className="pt-14 px-4 pb-4">
