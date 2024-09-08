a98f8152ab1bc3c495e96222e431f7f4b0daf8db
Ah-ae <kimaae@gmail.com>
2024-09-08 22:59:24 +0900

    Revert "test: Image 컴포넌트의 src에 문자열 경로 지정"
    This reverts commit a2611e48ec3e67f483664affda1cdff5f73ed2ab.

--- app/ui/kakao-login.tsx
+++ app/ui/kakao-login.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import KakaoIcon from '/public/kakao-icon.png';
 
 export default function KakaoLogin() {
   return (
@@ -7,7 +8,7 @@ export default function KakaoLogin() {
       href="/kakao/start"
       className="w-full h-10 flex-center gap-3 bg-[#FEE500] hover:bg-[#FADA0A] text-[#3C1E1E] font-medium rounded-md text-center transition-colors"
     >
-      <Image src="/kakao-icon.png" alt="KakaoTalk Icon" width={24} height={24} />
+      <Image src={KakaoIcon} alt="KakaoTalk Icon" width={24} height={24} />
       <span>카카오 로그인</span>
     </Link>
   );
