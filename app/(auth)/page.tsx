a2611e48ec3e67f483664affda1cdff5f73ed2ab
Ah-ae <kimaae@gmail.com>
2024-09-08 22:53:00 +0900

    test: Image 컴포넌트의 src에 문자열 경로 지정

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import HeroImage from '../../public/hero_256x256.png';
 
 export default function Home() {
   return (
     <div className="min-h-dvh px-6 py-12 flex flex-col justify-between items-center">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-        <Image src={HeroImage} alt="요즘뭐보니 대표 이미지" priority />
+        <Image src="/hero_256x256.png" alt="요즘뭐보니 대표 이미지" width={256} height={256} priority />
         <h1 className="my-2 text-4xl">요즘 뭐 보니</h1>
         <p className="text-2xl">친구들의 책장을 구경해 볼까요?</p>
       </div>
