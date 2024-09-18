3e8009d4414ce2e02f82250720f80354b3d256b3
Ahae KIM <115610668+Ah-ae@users.noreply.github.com>
2024-09-18 19:10:56 +0900

    refactor: StarRating 컴포넌트 optimistic mutation으로 변경 (#71)
    * refactor: StarRating 컴포넌트 optimistic mutation으로 변경 (#69)
    
    * fix: optimistic state update를 startTransition으로 wrapping
    
    - 상태 업데이트를 React.startTransition으로 감싸서 React에게 "이 업데이트는 낮은 우선순위의 작업이다"라는 정보를 제공

--- app/ui/books/star-rating.tsx
+++ app/ui/books/star-rating.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useOptimistic, startTransition } from 'react';
 import { StarIcon } from '@heroicons/react/24/outline';
 import { StarIcon as SolidStarIcon } from '@heroicons/react/24/solid';
 import { updateRating } from '@/(bookshelf)/mine/[id]/actions';
@@ -11,20 +12,41 @@ type Props = {
   rating: number;
   bookId: number;
 };
+
 export default function StarRating({ rating, bookId }: Props) {
+  const updateFn = (_: number, newRating: number) => newRating;
+  const [optimisticRating, setOptimisticRating] = useOptimistic(rating, updateFn);
+
   const onClick = async (index: number) => {
     let newRating = index + 1;
 
     // 별점이 1일 때 첫 번째 별을 다시 클릭하면 rating을 0으로 설정
-    if (rating === 1 && newRating === 1) {
+    if (optimisticRating === 1 && newRating === 1) {
       newRating = 0;
     }
 
-    await updateRating(bookId, newRating);
+    // 기존의 rating 값을 기억 (낙관적 업데이트 실패 시 롤백용)
+    const prevRating = optimisticRating;
+
+    // ui를 먼저 낙관적으로 업데이트
+    startTransition(() => {
+      setOptimisticRating(newRating);
+    });
+    try {
+      // 서버 업데이트
+      await updateRating(bookId, newRating);
+    } catch (error) {
+      console.error('Failed to update rating:', error);
+
+      // 서버 업데이트 실패 시 원래 상태로 롤백
+      startTransition(() => {
+        setOptimisticRating(prevRating);
+      });
+    }
   };
 
   const stars = Array.from({ length: MAX_RATING }, (_, index) => {
-    return index < rating ? (
+    return index < optimisticRating ? (
       <SolidStarIcon key={index} className={starIconStyles} onClick={() => onClick(index)} />
     ) : (
       <StarIcon key={index} className={starIconStyles} onClick={() => onClick(index)} />
