2b67652e9f78ed9b1d72880cb4fb21423294737e
Ahae KIM <115610668+Ah-ae@users.noreply.github.com>
2024-11-03 21:28:01 +0900

    update: dev 환경에서 회원가입 페이지 및 로그인 form 추가 (#78)

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,16 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+import { login } from './actions';
 import KakaoLogin from '@/ui/kakao-login';
+import Divider from '@/ui/divider';
+import Input from '@/ui/input';
+import Button from '@/ui/button';
+import { IS_DEV_MODE, PASSWORD_MIN_LENGTH } from '@/constants';
 
 export default function Login() {
+  const [state, dispatch] = useFormState(login, null);
+
   return (
     <div className="px-6 py-8 flex flex-col gap-10">
       <div className="flex flex-col gap-2 *:font-medium text-xl">
@@ -9,6 +19,24 @@ export default function Login() {
       </div>
 
       <KakaoLogin />
+
+      {IS_DEV_MODE && (
+        <>
+          <Divider />
+          <form action={dispatch} noValidate className="flex flex-col gap-3">
+            <Input name="username" type="text" placeholder="아이디" required errors={state?.fieldErrors.username} />
+            <Input
+              name="password"
+              type="password"
+              placeholder="비밀번호"
+              minLength={PASSWORD_MIN_LENGTH}
+              required
+              errors={state?.fieldErrors.password}
+            />
+            <Button text="로그인" />
+          </form>
+        </>
+      )}
     </div>
   );
 }
