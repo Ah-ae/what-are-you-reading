1ac0a02231ff67f85c10c65943c811c2d5c6929d
Ah-ae <kimaae@gmail.com>
2024-11-04 20:59:55 +0900

    feat: submit handler에서 alert-dialog 띄우기
    - root layout 조정 이유: dialog 컴포넌트가 body 요소를 건드려서 dialog가 열리면 body의 children에 layout shift가 발생

--- app/friends/add/page.tsx
+++ app/friends/add/page.tsx
@@ -1,16 +1,40 @@
+'use client';
+
+import { useState } from 'react';
 import HeaderLayout from '@/layout/header';
 import SearchForm from '@/ui/books/search-form';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui';
 
 export default function AddFriend({
   searchParams,
 }: {
   searchParams?: {
     query?: string;
-    target?: string;
   };
 }) {
   const query = searchParams?.query || '';
-  const target = searchParams?.target || '';
+  const [showAlertDialog, setShowAlertDialog] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(query);
+
+    if (query.length < 2) {
+      setShowAlertDialog(true);
+      return;
+    }
+
+    // TODO: DB query 해서 사용자 목록 보여주기
+  };
 
   return (
     <>
@@ -18,11 +42,27 @@ export default function AddFriend({
 
       {/* Note: `pt-12` - header height만큼 공간 확보 + page section의 자체 패딩 */}
       <section className="pt-16 px-3 pb-4">
-        <SearchForm query={query} target={target} />
+        <SearchForm onSubmit={handleSubmit} />
         <div className="h-40 flex-center">
           <p>친구의 이름을 검색하여 친구로 추가해 보세요.</p>
         </div>
       </section>
+
+      {showAlertDialog && (
+        <AlertDialog open={showAlertDialog} onOpenChange={(open) => !open && setShowAlertDialog(false)}>
+          <AlertDialogContent className="w-64 rounded-lg bg-white dark:bg-zinc-900 dark:text-gray-100">
+            <AlertDialogHeader>
+              <AlertDialogTitle className="text-gray-900 dark:text-inherit">알림</AlertDialogTitle>
+              <AlertDialogDescription>이름을 최소 2자 이상 입력해 주세요.</AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogAction className="text-white bg-main-theme-color hover:bg-green-700 dark:bg-blue-500 dark:hover:bg-blue-600 rounded-md">
+                확인
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className="dark">
-      <body
-        className={`mx-auto ${MAX_WIDTH} overflow-hidden shadow-xl bg-gray-100 text-neutral-900 dark:bg-zinc-950 dark:text-gray-100 ${inter.className}`}
-      >
-        <main className="min-h-screen bg-white dark:bg-zinc-900">{children}</main>
+      <body className={`bg-gray-100 text-neutral-900 dark:bg-zinc-950 dark:text-gray-100 ${inter.className}`}>
+        <div className={`mx-auto ${MAX_WIDTH} overflow-hidden shadow-xl`}>
+          <main className="min-h-screen bg-white dark:bg-zinc-900">{children}</main>
+        </div>
         <div id="portal" />
       </body>
     </html>
