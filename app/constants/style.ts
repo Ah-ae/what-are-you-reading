c5413d55769db4cb6b0c0fb0b9a1c2cad57cb0c8
Ah-ae <kimaae@gmail.com>
2026-02-18 14:49:21 +0900

    style: 책장 상세 border 색상 변경

--- app/constants/style.ts
+++ app/constants/style.ts
@@ -6,7 +6,7 @@ export const IMAGE_ASPECT_RATIO = {
 };
 export const SCALE_FACTOR = 4;
 
-export const containerStyles = 'px-4 flex flex-col gap-2 border-y-[1px] dark:border-neutral-700';
-export const itemStyles = 'py-3 border-b-[1px] last:border-b-0 dark:border-neutral-700';
+export const containerStyles = 'px-4 flex flex-col gap-2 border-y-[1px] border-neutral-200 dark:border-neutral-700';
+export const itemStyles = 'py-3 border-b-[1px] border-neutral-200 last:border-b-0 dark:border-neutral-700';
 export const beforePseudoElementStyles =
   'before:content-[attr(data-before)] before:absolute before:-translate-y-8 before:text-neutral-500';
